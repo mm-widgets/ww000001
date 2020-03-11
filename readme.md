2e5757547112f8ba053765d931c0d6e8c6f48a3f
taoqf <tao_qf@163.com>
2020-03-11 11:15:33 +0800

    init

--- readme.md
+++ readme.md
@@ -1,1 +1,218 @@
 # 树
+
+<!-- TOC -->
+
+- [1. 注意：](#1-注意)
+- [2. Attributes](#2-attributes)
+	- [2.1. `data`](#21-data)
+	- [2.2. `id-field`](#22-id-field)
+	- [2.3. `pid-field`](#23-pid-field)
+	- [2.4. `text-field`](#24-text-field)
+	- [2.5. `title-field`](#25-title-field)
+	- [2.6. `icon`](#26-icon)
+	- [2.7. `icon-field`](#27-icon-field)
+	- [2.8. `dots`](#28-dots)
+	- [2.9. `checkbox`](#29-checkbox)
+	- [2.10. `disable_undetermined_state`](#210-disable_undetermined_state)
+	- [2.11. `state`](#211-state)
+	- [2.12. `auto-open`](#212-auto-open)
+	- [2.13. `auto-close`](#213-auto-close)
+- [3. Methods](#3-methods)
+	- [3.1. `get_data_by_id`](#31-get_data_by_id)
+	- [3.2. `get_node_by_id`](#32-get_node_by_id)
+	- [3.3. `search`](#33-search)
+	- [3.4. `clear_search`](#34-clear_search)
+	- [3.5. `close_all`](#35-close_all)
+	- [3.6. `open_all`](#36-open_all)
+	- [3.7. `open`](#37-open)
+	- [3.8. `close`](#38-close)
+	- [3.9. `get_checked`](#39-get_checked)
+	- [3.10. `check_all`](#310-check_all)
+	- [3.11. `uncheck_all`](#311-uncheck_all)
+	- [3.12. `check_node`](#312-check_node)
+- [4. Events](#4-events)
+	- [4.1. feidao-web事件绑定示例](#41-feidao-web事件绑定示例)
+	- [4.2. `mmwe-tree-select-node`](#42-mmwe-tree-select-node)
+	- [4.3. `mmwe-tree-activate-node`](#43-mmwe-tree-activate-node)
+- [5. Example](#5-example)
+	- [5.1. 完整示例](#51-完整示例)
+
+<!-- /TOC -->
+
+## 1. 注意：
+
+需要在gulpfile.js文件的webpack命令中添加以下代码:
+
+```js
+...
+module:{
+	...
+},
+plugins: [
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		jQuery: "jquery"
+	})
+]
+}, webpack))
+...
+```
+
+## 2. Attributes
+
+### 2.1. `data`
+
+必要属性，必须为一个数组，并且至少有三个字段
+
+```html
+<mm-000001 checkbox dots icon="./images/45_70x70.jpg" id-field="no" text-field="name" pid-field="parent" title-field="alias" icon-field="img" data='[{"no":1,"parent":"root","name":"1", "alias":"title"},{"no":2,"parent":"0","name":"2"},{"no":"11","parent":"1","name":"11"},{"no":"12","parent":"1","name":"12", "img":"https://avatars1.githubusercontent.com/u/15901911?s=40&v=4"},{"no":"111","parent":"11","name":"111"},{"no":"112","parent":"11","name":"112"},{"no":"211","parent":"2","name":"211"}]'></mm-000001>
+```
+
+如需刷新数据，请使用脚本重新设置data属性
+
+```ts
+widget.setAttribute('data', '[{"no":1,"parent":"root","name":"1", "alias":"ddddddddddd"},{"no":2,"parent":"0","name":"2"},{"no":"11","parent":"1","name":"444"},{"no":"12","parent":"1","name":"12"},{"no":"111","parent":"11","name":"111", "no_checkbox": true},{"no":"112","parent":"11","name":"112", "no_checkbox": true},{"no":"211","parent":"2","name":"211", "no_checkbox": true}]');
+```
+
+### 2.2. `id-field`
+
+提供的数据中可以作为`id`的字段名,该字段用来标识每一个树结点。默认值为`id`
+
+### 2.3. `pid-field`
+
+提供的数据中可以作为`pid`的字段名，该字段用来标识该结点的父结点。默认值为`pid`
+
+### 2.4. `text-field`
+
+提供的数据中可以作为`text`的字段名，该字段用来在树结点上显示。默认值为`text`
+
+### 2.5. `title-field`
+
+提供的数据中可以作为`title`的字段名，该字段用来在树结点上标示`title`属性，鼠标在上面的时候弹出提示信息，当需要显示更多内容的时候有用。默认值为`title`
+
+### 2.6. `icon`
+
+提供一个图片地址，用来替换默认图标显示,如果未定义属性值或属性值为字符串，则使用默认图标显示
+
+### 2.7. `icon-field`
+
+提供的数据中可以作为`icon`的字段名，该字段用来在树结点上显示图标。默认值为`icon`。只有设置了`icon`属性时起使用
+
+### 2.8. `dots`
+
+是否显示结点左边的虚线
+
+### 2.9. `checkbox`
+
+是否显示复选框,如果显示复选框的话，点击节点不会展开或关闭
+
+### 2.10. `disable_undetermined_state`
+
+是否禁用三态复选框，注意：`checkbox`为true时才有效
+
+### 2.11. `state`
+
+是否保存状态
+
+### 2.12. `auto-open`
+
+点击结点是否进行展开
+
+### 2.13. `auto-close`
+
+点击结点是否进行关闭
+
+## 3. Methods
+
+### 3.1. `get_data_by_id`
+
+通过id获取数据
+
+### 3.2. `get_node_by_id`
+
+通过结点id获取结点对象
+
+### 3.3. `search`
+
+在树中进行搜索
+
+### 3.4. `clear_search`
+
+解除搜索状态，还原完整树结点显示
+
+### 3.5. `close_all`
+
+折叠全部结点
+
+### 3.6. `open_all`
+
+打开全部结点
+
+### 3.7. `open`
+
+打开某个结点
+
+### 3.8. `close`
+
+折叠某个结点
+
+### 3.9. `get_checked`
+
+获取选中的数据
+
+### 3.10. `check_all`
+
+全部选中
+
+### 3.11. `uncheck_all`
+
+全部不选
+
+### 3.12. `check_node`
+
+选中一个或多个结点
+
+## 4. Events
+
+### 4.1. feidao-web事件绑定示例
+
+```html
+<mm-000001 data-feidao-actions="mmwe-tree-select-node:a001"></mm-000001>
+```
+
+### 4.2. `mmwe-tree-select-node`
+
+参数属性`data`中数据格式如下
+
+```ts
+{
+	data: any;
+	id: string;
+	parent: string;
+	parents: string[];
+}
+```
+
+**注意**此事件在初始化时，如果该控件不是第一次使用，也会被调用，如果不希望该事件在初始化时被调用，请使用`mmwe-tree-activate-node`
+
+### 4.3. `mmwe-tree-activate-node`
+
+参数属性`data`中数据格式如下
+
+```ts
+{
+	data: any;
+	id: string;
+	parent: string;
+	parents: string[];
+}
+```
+
+## 5. Example
+
+### 5.1. 完整示例
+
+```html
+<mm-000001 id="widget" checkbox node_open node_close dots icon="./images/45_70x70.jpg" id-field="no" text-field="name" pid-field="parent" title-field="alias" icon-field="img" data='[{"no":1,"parent":"root","name":"1", "alias":"ffffffffffffffffffffffff"},{"no":2,"parent":"0","name":"2"},{"no":"11","parent":"1","name":"444"},{"no":"12","parent":"1","name":"12", "img":"https://avatars1.githubusercontent.com/u/15901911?s=40&v=4"},{"no":"111","parent":"11","name":"111", "no_checkbox": true},{"no":"112","parent":"11","name":"112", "no_checkbox": true},{"no":"211","parent":"2","name":"211", "no_checkbox": true}]' data-mm-action="mmwe-tree-select-node:a001,mmwe-tree-activate-node:a002"></mm-000001>
+<input id="btn" type="button" value="test">
+```
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import on, { emit } from '@mmstudio/on';
+import { emit } from '@mmstudio/on';
+import $ from 'jquery';
+import 'jstree';
 
-const no = 'mm-000001';
+const no = 'fd-w000001';
 
 /**
  * :host表示选中当前组件,使用all:initial;属性,
  * 当外部有css样式要越过shadowDom边界时将该css样式重置为初始值,从而不在影响shaowDom内部的样式.
  * 参考链接https://developers.google.cn/web/fundamentals/web-components/shadowdom#reset,
- * 使用 000001 作为所有样式类的前缀可以作为前置限定，从而避免某些浏览器对shadowdom支持不好，样式错乱的问题,
- * tpl 中可以添加link标签，但是最好使用公网可以访问到的cdn连接，且一定要保证版本号固定，如<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/jstree@3.3.7/dist/themes/default/style.min.css" />
  */
 const tpl = `
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/jstree@3.3.7/dist/themes/default/style.min.css" />
 <style>
 .${no} {
 	width: 100%;
@@ -17,47 +18,308 @@ const tpl = `
 	overflow: hidden;
 	position: relative;
 }
+.${no} .no_checkbox > i.jstree-checkbox {
+	display:none;
+}
 :host {
 	all: initial;
 }
 </style>
 
 <div class="${no}">
-	<div id="n01">Hello <a href="https://mm-edu.gitee.io/">mmstudio</a></div>
+	<div id="tree"></div>
 </div>
 `;
 
-export default class Widget extends HTMLElement {
+interface Item {
+	icon: string;
+	data: unknown;
+	id: string;
+	parent: string;
+	text: string;
+}
+
+export default class Tree extends HTMLElement {
+	private jstree: JSTree;
+	private m_id!: Map<string, Item>;
+	private data!: Item[];
 	public constructor() {
 		super();
 		const dom = this.attachShadow({ mode: 'closed' });
 		dom.innerHTML = tpl;
-		const s = this.getAttribute('sattr');
-		const n = dom.querySelector<HTMLDivElement>('#n01')!;
-		on(n, 'click', () => {
-			emit(this, 'mmwe-click', true, true);
+		const checkbox = get_boolean_attribute(this, 'checkbox');
+		const auto_open = get_boolean_attribute(this, 'auto-open');
+		const auto_close = get_boolean_attribute(this, 'auto-close');
+		const state = get_boolean_attribute(this, 'state');
+		const dots = get_boolean_attribute(this, 'dots');
+		const three_state = !get_boolean_attribute(this, 'disable_undetermined_state');
+		const icon = this.getAttribute('icon') as string;
+		const tree = dom.getElementById('tree') as HTMLDivElement;
+		const data = JSON.parse(this.getAttribute('data') || '[]');
+		this.translate_data(data);
+		const el = $(tree);
+		const plugins = ['search'];
+		if (checkbox) {
+			plugins.push('checkbox');
+		}
+		if (state) {
+			plugins.push('state');
+		}
+		el.jstree({
+			checkbox: {
+				three_state
+			},
+			core: {
+				data: (_obj: unknown, cb: (d: Item[]) => void) => {
+					cb(this.data);
+				},
+				dblclick_toggle: false,
+				worker: false	// ! disable worker to avoid error after webpack
+			},
+			plugins
 		});
-		emit(this, 'mmwe-init', true, true, {
-			data: {
-				id: this.id
+		this.jstree = el.jstree(true);
+		// el.on('click', '.jstree-anchor', (e) => {
+		// 	this.jstree.toggle_node(e.target);
+		// });
+		el.on('activate_node.jstree', (_e, evt_data) => {
+			const node = evt_data.node;
+			if (node.children.length > 0) {
+				if (this.jstree.is_open(node) && auto_close) {
+					this.close(node.id);			// 结点是展开的，则进行关闭
+				} else if (this.jstree.is_closed(node) && auto_open) {
+					this.open(node.id);				// 结点是关闭的，则进行展开
+				}
 			}
+			emit(this, 'fdwe-tree-activate-node', true, true, {
+				data: {
+					data: node.data,
+					id: node.id,
+					parent: node.parent,
+					parents: node.parents
+				}
+			});
 		});
-		alert(`widget ${no} inited, attribte: sattr = ${s} `);
+		el.on('select_node.jstree', (_e, evt_data) => {
+			const node = evt_data.node;
+			emit(this, 'fdwe-tree-select-node', true, true, {
+				data: {
+					data: node.data,
+					id: node.id,
+					parent: node.parent,
+					parents: node.parents
+				}
+			});
+		});
+		if (icon === null) {
+			this.jstree.hide_icons();
+		}
+		if (!dots) {
+			this.jstree.hide_dots();
+		}
+	}
+	public get_data_by_id(id: string) {
+		const item = this.m_id.get(id);
+		if (item) {
+			return item.data;
+		}
+		return undefined;
+	}
+	public get_node_by_id(id: string) {
+		return this.jstree.get_node(id);
+	}
+	public search(str: string) {
+		return this.jstree.search(str);
+	}
+	public clear_search() {
+		return this.jstree.clear_search();
+	}
+	public close_all() {
+		return this.jstree.close_all();
+	}
+	public open_all() {
+		return this.jstree.open_all();
+	}
+	public open(id: string) {
+		return this.jstree.open_node(id);
+	}
+	public close(id: string) {
+		return this.jstree.close_node(id);
+	}
+	public get_checked() {
+		return this.jstree.get_checked(true).map((it) => {
+			return it.data;
+		});
+	}
+	public get_top_checked() {
+		return this.jstree.get_top_checked(false) as string[];
+	}
+	public get_bottom_checked() {
+		return this.jstree.get_bottom_checked(false) as string[];
+	}
+	public check_all() {
+		this.jstree.check_all();
+	}
+	public uncheck_all() {
+		this.jstree.uncheck_all();
+	}
+	public check_node(id: string | string[]) {
+		this.jstree.check_node(id, undefined);
+	}
+	public uncheck_node(id: string | string[]) {
+		this.jstree.uncheck_node(id, undefined);
+	}
+	public is_checked(id: string) {
+		this.jstree.is_checked(id);
+	}
+	public select_node(id: string | string[]) {
+		return this.jstree.select_node(id);
+	}
+	public deselect_node(id: string | string[]) {
+		return this.jstree.deselect_node(id);
+	}
+	public select_all() {
+		return this.jstree.select_all();
+	}
+	public deselect_all() {
+		return this.jstree.deselect_all();
+	}
+	public is_selected(id: string) {
+		return this.jstree.is_selected(id);
+	}
+	public get_selected() {
+		return this.jstree.get_selected() as string[];
+	}
+	public get_top_selected() {
+		return this.jstree.get_top_selected() as string[];
+	}
+	public get_bottom_selected() {
+		return this.jstree.get_bottom_selected() as string[];
+	}
+	public disable_checkbox(id: string | string[]) {
+		return this.jstree.disable_checkbox(id);
+	}
+	public enable_checkbox(id: string | string[]) {
+		return this.jstree.enable_checkbox(id);
 	}
 	/**
-	 * 方法注释
-	 * @param param 参数注释
+	 * get the path to a node, either consisting of node texts, or of node IDs
+	 * @param id nodeid
+	 * @param ids if set to true build the path using ID, otherwise node text is used
 	 */
-	public method01(param: string) {
-		window.console.debug('this is a public method', param);
-		this.method02(param);
+	public get_path(id: string | string[], ids: boolean) {
+		return this.jstree.get_path(id, '/', ids);
 	}
-	private method02(param: string) {
-		alert(`This is a private method: ${param}`);
+	public get_parent(id: string) {
+		return this.jstree.get_parent(id);
+	}
+	public get_children_dom(id: string) {
+		return this.jstree.get_children_dom(id);
+	}
+	public is_parent(id: string) {
+		return this.jstree.is_parent(id);
+	}
+	public is_leaf(id: string) {
+		return this.jstree.is_leaf(id);
+	}
+	public is_node_open(id: string) {
+		return this.jstree.is_open(id);
+	}
+	public is_node_closed(id: string) {
+		return this.jstree.is_closed(id);
+	}
+	public is_disabled(id: string) {
+		return this.jstree.is_disabled(id);
+	}
+	public enable_node(id: string) {
+		return this.jstree.enable_node(id);
+	}
+	public disable_node(id: string) {
+		return this.jstree.disable_node(id);
+	}
+	public is_hidden(id: string) {
+		return this.jstree.is_hidden(id);
+	}
+	public hide_node(id: string, skip_redraw: boolean) {
+		return this.jstree.hide_node(id, skip_redraw);
+	}
+	public show_node(id: string, skip_redraw: boolean) {
+		return this.jstree.show_node(id, skip_redraw);
+	}
+	public hide_all(skip_redraw: boolean) {
+		return this.jstree.hide_all(skip_redraw);
+	}
+	public show_all(skip_redraw: boolean) {
+		return this.jstree.show_all(skip_redraw);
+	}
+	public get_text(id: string) {
+		return this.jstree.get_text(id);
+	}
+	public set_text(id: string | string[], text: string) {
+		return this.jstree.rename_node(id, text);
+	}
+	public get_json(id: string) {
+		return this.jstree.get_json(id, undefined, false);
+	}
+	public delete_node(id: string) {
+		return this.jstree.delete_node(id);
+	}
+	public hide_icon(id: string) {
+		return this.jstree.hide_icon(id);
+	}
+	public show_icon(id: string) {
+		return this.jstree.show_icon(id);
+	}
+	private translate_data(value: { [field: string]: string; }[]) {
+		const icon = this.getAttribute('icon') as string;
+		const id_field = this.getAttribute('id-field') || 'id';
+		const pid_field = this.getAttribute('pid-field') || 'pid';
+		const text_field = this.getAttribute('text-field') || 'text';
+		const title_field = this.getAttribute('title-field') || 'title';
+		const icon_field = this.getAttribute('icon-field') || 'icon';
+		const m_id = this.m_id = new Map<string, Item>();
+		this.data = value.map((it) => {
+			const item = {
+				a_attr: {
+					class: it.no_checkbox ? 'no_checkbox' : '',
+					title: it[title_field] || it[text_field]
+				},
+				data: it,
+				icon: it[icon_field] || icon,
+				id: it[id_field].toString(),
+				parent: it[pid_field].toString(),
+				text: it[text_field]
+			} as Item;
+			m_id.set(item.id, item);
+			return item;
+		}).map((it) => {
+			if (m_id.has(it.parent)) {
+				return { ...it };
+			}
+			return { ...it, parent: '#' };
+		});
+		return this.data;
+	}
+	public set_data(value: { [field: string]: string; }[]) {
+		// reset local variables
+		this.translate_data(value);
+		this.jstree.refresh(false, false);
 	}
 }
 
 if (!window.customElements.get(no)) {
-	window.customElements.define(no, Widget);
+	window.customElements.define(no, Tree);
 }
 
+function get_boolean_attribute(node: HTMLElement, attribute: string) {
+	if (node.hasAttribute(attribute)) {
+		const value = node.getAttribute(attribute);
+		if (value) {
+			return value.toLocaleLowerCase() !== 'false';
+		}
+		return true;
+
+	}
+	return false;
+
+}
